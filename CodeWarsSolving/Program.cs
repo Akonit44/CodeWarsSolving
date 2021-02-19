48ce1d0781d0de739b0875820e797754b0babd79
Akonit44 <38272921+Akonit44@users.noreply.github.com>
2021-02-20 00:16:09 +0300

    Продолжил писать метод выдающий три самые частые строки

--- CodeWarsSolving/Program.cs
+++ CodeWarsSolving/Program.cs
@@ -14,14 +14,25 @@ namespace CodeWarsSolving
     {
         static void Main(string[] args)
         {
-            string z = "s sffs, oprit fsdg";
+            string z = "s sffs, oprit sffs fsdg";
             List<string> a = new List<string>();
-            Console.WriteLine(a.Count);
+            
             a.AddRange(z.ToLower().Replace(",", "").Split(' '));
-            foreach (string elem in a)
+            string toCount = "";
+            int[] topThree = new int[3];
+            for (int i = 0; i < a.Count;i++)
             {
-                Console.WriteLine(elem + 10);
+                toCount = a[i];
+                foreach (string elem in a)
+                {
+                    if(elem == toCount)
+                    {
+                        topThree[0]++;
+                    }
+                }
+                a.RemoveAll((toCount) => { return true; });
             }
+            Akonit44_HelpMethods.showMass(topThree);
             
         }
             
